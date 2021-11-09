137e11a141b1d172baac03e895689132f780a99c
Skydiver412 <bkunkel412@yahoo.com>
2021-11-08 19:53:22 -0500

    Input Controls

---
+++ Assets/Heli/Code/Scripts/Input/IP_BaseHeli_Input.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IndiePixel
+{
+    public class IP_BaseHeli_Input : MonoBehaviour
+    {
+        #region Variables
+        [Header("Base Input Properties")]
+        public float vertical = 0f;
+        public float horizontal = 0f;
+        #endregion
+
+        #region Built-in
+
+        // Update is called once per frame
+        void Update()
+        {
+            HandleInputs();
+        }
+        #endregion
+
+        #region Custom
+        protected virtual void HandleInputs()
+        {
+            vertical = Input.GetAxis("Vertical");
+            horizontal = Input.GetAxis("Horizontal");
+        }
+        #endregion
+    }
+}
+
